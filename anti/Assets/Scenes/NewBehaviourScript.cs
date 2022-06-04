675e1b01efe96791fbb4375957bf9a29fe351294
Nathank0126753 <nathankim012753@gmail.com>
2022-06-03 21:18:20 -0400

    gravity and collision

--- anti/Assets/Scenes/NewBehaviourScript.cs
+++ anti/Assets/Scenes/NewBehaviourScript.cs
@@ -5,35 +5,62 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    private Rigidbody2D rb2d;
+    
+    private float moveSpeed;
+    private float jumpForce;
+    private bool isJumping;
+    private float moveHorizontal;
+    private float moveVertical;
     // Start is called before the first frame update
     void Start()
     {
+        //lowercase gameObject references this.gameobject
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        moveSpeed = 5f;
+        jumpForce = 60f;
+        isJumping = false;
         
     }
-    public float speed = 10.0f;
-    public float rotationSpeed = 100.0f;
-
     // Update is called once per frame
+
     void Update()
     {
-        float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-
+        moveHorizontal = Input.GetAxisRaw("Horizontal"); //are they clicking a/d or left/right can be changed in project settings
+        //if presing a/left, it returns -1, if pressing d/right, it returns 1, otherwise it returns 0
+        moveVertical = Input.GetAxisRaw("Vertical");
         // Make it move 10 meters per second instead of 10 meters per frame...
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
-
         // Move translation along the object's z-axis
-        transform.Translate(0, translation, 0);
-
         // Rotate around our y-axis
-        transform.Rotate(0, rotation, 0);
+        //transform.Rotate(0, rotation, 0);
         //float x = Input.GetAxis("Horizontal");
         //float y = Input.GetAxis("Vertical");
         //if (Input.GetKeyDown(KeyCode.Space)){
           //  Debug.Log("saweaweawee" + "x pos: " + x + " y pos: " + y);
             //transform.position = new Vector3(x+3000, y+3000, 2);
         //}
+    }
+    void FixedUpdate(){
+        //have movement code here bc we need unitys physics to run
+        //instead of 0, getaxisraw technically returns only 1/-1/0, but its just margin of error for more compledx things
+        if(moveHorizontal > 0.1f || moveHorizontal < -0.1f){
+            //normally, you might also multiply Time.Deltatime/Time.fixedDeltaTime, but addforce already does that so no need too
+            rb2d.AddForce(new Vector2(moveHorizontal*moveSpeed, 0f), ForceMode2D.Impulse); //vector 2 is x/y axis, vector 3 is x/y/z axises
+
+        }
+        if (!isJumping && moveVertical > 0.1f){
+            rb2d.AddForce(new Vector2(0f, moveVertical*jumpForce), ForceMode2D.Impulse);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision){
+        if (collision.gameObject.tag == "Platform"){
+            isJumping = false;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision){
+        if (collision.gameObject.tag == "Platform"){
+            isJumping = true;
+        }
     }
     
 }
